684e54f49fc3b74e9460e37bbf3bb855c23cd27f
David Blayney <david@davblayn.com>
2013-04-14 15:38:21 +0800

    Create Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boarderline::Application.config.secret_token = 'a1a70b1f9d25a36485a8aa518e2622f4d35c20f951da5aeb3b485509090660ebf5021eca214ae2aeaa9068ddba523ae2ac5037d3ae088d919f2910927c259e00'
