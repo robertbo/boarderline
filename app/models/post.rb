25e121541350d9afe5c92cd214236d01bf51c1ee
David Blayney <david@davblayn.com>
2013-04-15 21:25:40 +0800

    Add tests for new news posts; closes #8 and #9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :content, :title
+
+  validates :title, presence: true,
+                    length: { maximum: 30 }
+
+  validates :content, presence: true,
+                      length: { minimum: 20 }
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "new posts title" do
+    should "exist" do
+      post = Post.new
+      post.content = posts(:one).content
+
+      assert !post.save
+      assert !post.errors[:title].empty?
+    end
+
+    should "contain less than 30 characters" do
+      post = Post.new
+      post.content = posts(:one).content
+
+      post.title = "This is a really long title that is longer than 30 characters"
+
+      assert !post.save
+      assert !post.errors[:title].empty?
+    end
+  end
+
+  context "new posts content" do
+    should "exist" do
+      post = Post.new
+      post.title = posts(:one).title
+
+      assert !post.save
+      assert !post.errors[:content].empty?
+    end
+
+    should "contain at least 20 characters" do
+      post = Post.new
+      post.title = posts(:one).title
+
+      post.content = "Less than 20"
+
+      assert !post.save
+      assert !post.errors[:content].empty?
+    end
+  end
 end
