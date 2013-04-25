c2fe92bcb599d837bbbe376d79f053d617f62fb8
David Blayney <david.b359@gmail.com>
2013-04-25 02:46:33 -0400

    Add presence validations
    Validates the presence of first name, last name and email address

--- test/unit/admin_test.rb
+++ test/unit/admin_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "when adding an admin account" do
+    should "enter a first name" do
+      admin = Admin.new
+      assert !admin.save
+      assert !admin.errors[:first_name].empty?
+    end
+    
+    should "enter a last name" do
+      admin = Admin.new
+      assert !admin.save
+      assert !admin.errors[:last_name].empty?
+    end
+    
+    should "enter an email address" do
+      admin = Admin.new
+      assert !admin.save
+      assert !admin.errors[:email].empty?
+    end
+  end
 end
