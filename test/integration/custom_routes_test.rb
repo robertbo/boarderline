78fbaf8f858f86b33199b4974050b4fdd5678afb
David Blayney <david.b359@gmail.com>
2013-05-15 07:50:46 -0400

    Convert /news text to use Should-Context

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class CustomRoutesTest < ActionDispatch::IntegrationTest
-  test "that /news gets the all news posts page" do
-    get '/news'
-    assert_response :success
+  context "news" do
+    should "should display index on /news" do
+      get '/news'
+      assert_response :success
+    end
   end
 end
