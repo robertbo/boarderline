a3dfbe93748e438b1730e91b576b1345613f0543
David Blayney <david.b359@gmail.com>
2013-05-15 07:52:22 -0400

    Fix redundancy in test name

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CustomRoutesTest < ActionDispatch::IntegrationTest
   context "news" do
-    should "should display index on /news" do
+    should "display index on /news" do
       get '/news'
       assert_response :success
     end
