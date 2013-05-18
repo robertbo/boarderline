67ed06b7b6e507322903116604f5c8ab39f57acd
David Blayney <david.b359@gmail.com>
2013-05-18 08:07:35 -0400

    Create database association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :content, :title
+  
+  belongs_to :user
 
   validates :title, presence: true,
                     length: { maximum: 100 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
   # attr_accessible :title, :body
   
+  has_many :posts
+  
   validates :first_name, :presence => true
   
   validates :last_name, :presence => true
