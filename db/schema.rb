a1711f50c17a53a09b89e0d2d9b659f3a77a8700
David Blayney <david.b359@gmail.com>
2013-05-18 05:31:24 -0400

    Add user_id conumn to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512092628) do
+ActiveRecord::Schema.define(:version => 20130518092730) do
 
   create_table "posts", :force => true do |t|
-    t.string   "author"
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
