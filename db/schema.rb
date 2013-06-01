ac35d630aeb36c0a4b0ad777f5bca4624941804a
David Blayney <david.b359@gmail.com>
2013-06-01 19:58:16 +0800

    Add admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518092730) do
+ActiveRecord::Schema.define(:version => 20130601115423) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20130518092730) do
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
