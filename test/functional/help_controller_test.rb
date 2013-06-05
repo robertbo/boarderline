21e1876c2841b438e406982f9f22238345b54b2d
David Blayney <david.b359@gmail.com>
2013-06-05 20:22:12 +0800

    Rewrite tests for user and admin signed in

--- test/functional/help_controller_test.rb
+++ test/functional/help_controller_test.rb
@@ -1,13 +1,47 @@
 require 'test_helper'
 
 class HelpControllerTest < ActionController::TestCase
-  test "should get markdown" do
-    get :markdown
-    assert_response :success
+  context "when not signed in" do
+    should "get markdown" do
+      get :markdown
+      assert_response :success
+    end
+
+    should "get welcome" do
+      get :welcome
+      assert_response :success
+    end
   end
-  
-  test "should get welcome" do
-    get :welcome
-    assert_response :success
+
+  context "when user signed in" do
+    setup do
+      sign_in users(:user1)
+    end
+
+    should "get markdown" do
+      get :markdown
+      assert_response :success
+    end
+
+    should "get welcome" do
+      get :welcome
+      assert_response :success
+    end
+  end
+
+  context "when admin signed in" do
+    setup do
+      sign_in users(:admin1)
+    end
+
+    should "get markdown" do
+      get :markdown
+      assert_response :success
+    end
+
+    should "get welcome" do
+      get :welcome
+      assert_response :success
+    end
   end
 end
