e37aed5c9d132e2900efb7c56041f2edcc294a8d
David Blayney <david.b359@gmail.com>
2013-06-05 23:29:08 +0800

    Clean up help controller test

--- test/functional/help_controller_test.rb
+++ test/functional/help_controller_test.rb
@@ -1,77 +1,23 @@
 require 'test_helper'
 
-class HelpControllerTest < ActionController::TestCase
-  context "when not signed in" do
-    should "get index" do
-      get :index
-      assert_response :success
-    end
-
-    should "get markdown" do
-      get :markdown
-      assert_response :success
-    end
-
-    should "get welcome" do
-      get :welcome
-      assert_response :success
-    end
-
-    should "get news" do
-      get :news
-      assert_response :success
-    end
+class HelpControllerTest < ActionController::TestCaseo
+  should "get index" do
+    get :index
+    assert_response :success
   end
 
-  context "when user signed in" do
-    setup do
-      sign_in users(:user1)
-    end
-
-    should "get index" do
-      get :index
-      assert_response :success
-    end
-
-    should "get markdown" do
-      get :markdown
-      assert_response :success
-    end
-
-    should "get welcome" do
-      get :welcome
-      assert_response :success
-    end
-
-    should "get news" do
-      get :news
-      assert_response :success
-    end
+  should "get markdown" do
+    get :markdown
+    assert_response :success
   end
 
-  context "when admin signed in" do
-    setup do
-      sign_in users(:admin1)
-    end
+  should "get welcome" do
+    get :welcome
+    assert_response :success
+  end
 
-    should "get index" do
-      get :index
-      assert_response :success
-    end
-
-    should "get markdown" do
-      get :markdown
-      assert_response :success
-    end
-
-    should "get welcome" do
-      get :welcome
-      assert_response :success
-    end
-
-    should "get news" do
-      get :news
-      assert_response :success
-    end
+  should "get news" do
+    get :news
+    assert_response :success
   end
 end
