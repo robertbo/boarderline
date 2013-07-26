2d49cd360c0ba9bf0e66a536887fbbd409ef7019
David Blayney <david.b359@gmail.com>
2013-07-26 20:52:53 +0800

    Update idea database migration
    Current fields are:
    * Author (string)
    * Title (string)
    * Description (text)
    * Response (text)
    * Status (string)

--- db/migrate/20130726124911_create_ideas.rb
+++ db/migrate/20130726124911_create_ideas.rb
@@ -1,10 +1,11 @@
 class CreateIdeas < ActiveRecord::Migration
   def change
     create_table :ideas do |t|
-      t.string :status
+      t.string :author
       t.string :title
       t.text :description
       t.text :response
+      t.text :status
 
       t.timestamps
     end
